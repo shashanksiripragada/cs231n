bc2f66074b7e51d39e93c88cc35d6e9605133b0d
Shashank Siripragada <siripragadashashank@gmail.com>
2018-12-03 16:31:48 +0530

    updated a2 fc

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -25,7 +25,8 @@ def affine_forward(x, w, b):
     # TODO: Implement the affine forward pass. Store the result in out. You   #
     # will need to reshape the input into rows.                               #
     ###########################################################################
-    pass
+    x = x.reshape(x.shape[0],-1)
+    out = np.dot(x,w) + b
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
@@ -54,7 +55,14 @@ def affine_backward(dout, cache):
     ###########################################################################
     # TODO: Implement the affine backward pass.                               #
     ###########################################################################
-    pass
+    flat_x = x.reshape(x.shape[0],-1)
+    
+    dx = np.dot(dout,w.T).reshape(x.shape)
+    
+    dw = np.dot(flat_x.T,dout)
+    
+    db = np.sum(dout,axis=0)
+    
     ###########################################################################
     #                             END OF YOUR CODE                            #
     ###########################################################################
